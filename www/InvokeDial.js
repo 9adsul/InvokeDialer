82d1739b0ff2558b0b04208faba7b8f0a13a4fe1
Navnath Adsul <9adsul@users.noreply.github.com>
2019-10-10 15:33:35 +0530

    Delete InvokeDial.js

--- www/InvokeDial.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    invokeDial : function (number, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "InvokeDial", "invokeDial", [number]);
-    }
-};
